7012831ccca187309f0dee620f08f6e24dbcf2a2
Naga-venkatesh21 <viswanadhulavenkatesh94@gmail.com>
2025-09-20 11:19:55 +0530

    PickStart

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ function App() {
           <Navbar />
           
             <Routes>
-              
               <Route path='/' element={<HomePage/>}/>
               <Route path='/services' element={<Services/>}/>
             </Routes>
